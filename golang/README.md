31e084e9641a32713eff48c51a074fb1f5ff5588
yasushi.ando <yasushiando@90200983N.local>
2011-09-30 14:52:47 +0900

    add golang

---
+++ golang/README.md
@@ -0,0 +1,15 @@
+# 実行手順
+
+1. mixi Developer DashboardでリダイレクトURLを [http://localhost:8080] にしたアプリを登録（ポート番号は任意）
+2. config.jsonを作成
+    {
+        "client_id":"YOUR CLIENT ID",
+        "client_secret":"YOUR SECRET",
+        "redirect_port":"8080"
+    }
+3. コンパイルしてサーバー起動
+    $ 6g mixi.go
+    $ 6l mixi.6
+    $ 6.out
+    Please open http://localhost:8080 on a web browser.
+4. ブラウザで [http://localhost:8080] を開く
---
+++ golang/mixi.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+  "http"
+  "io/ioutil"
+  "json"
+  "log"
+  "net"
+  "os"
+  "strings"
+  "template"
+)
+
+type friend struct {
+  DisplayName string
+  ThumbnailUrl string
+  ProfileUrl string
+}
+
+type tokens struct {
+  AccessToken string
+  RefreshToken string
+}
+
+const (
+  AUTHORIZE_URL_BASE = "https://mixi.jp/connect_authorize.pl?response_type=code&scope=r_profile&client_id="
+  TOKENS_ENDPOINT = "https://secure.mixi-platform.com/2/token"
+  FRIEND_LIST_ENDPOINT= "http://api.mixi-platform.com/2/people/@me/@friends"
+  CONFIG_FILENAME = "config.json"
+  TOKENS_FILENAME = "tokens.txt"
+  TEMPLATE = `
+<html>
+  <head><title>Friend Lit</title></head>
+  <body>
+    <ul>
+    {.repeated section Friends}
+      <li>
+        <img src="{ThumbnailUrl}" width="30" height="30" />
+        <a href="{ProfileUrl}">{DisplayName}</a>
+      </li>
+    {.end}
+    </ul>
+  </body>
+</html>
+`
+)
+
+var config map[string]string
+var currentTokens *tokens
+
+func NewTokens(accessToken string, refreshToken string) (*tokens) {
+  return &tokens{accessToken, refreshToken}
+}
+
+func RestoreTokens() (*tokens) {
+  if bytes, err := ioutil.ReadFile(TOKENS_FILENAME); err == nil {
+    pair := strings.Split(string(bytes), "\n", -1)
+    return &tokens{pair[0], pair[1]}
+  }
+  return nil
+}
+
+func (t *tokens)store() {
+  ioutil.WriteFile(TOKENS_FILENAME, []byte(t.AccessToken +
+    "\n" + t.RefreshToken), 0666)
+}
+
+func authorizeCode(authCode string) (err os.Error) {
+  return updateTokens(map[string][]string{
+    "grant_type":{"authorization_code"},
+    "client_id":{config["client_id"]},
+    "client_secret":{config["client_secret"]},
+    "code":{authCode},
+    "redirect_uri":{"http://localhost:" + config["redirect_port"]},
+  })
+}
+
+func refreshToken() (err os.Error) {
+  return updateTokens(map[string][]string{
+    "grant_type":{"refresh_token"},
+    "client_id":{config["client_id"]},
+    "client_secret":{config["client_secret"]},
+    "refresh_token":{currentTokens.RefreshToken},
+  })
+}
+
+func updateTokens(params map[string][]string) (err os.Error) {
+  println("Call: " + TOKENS_ENDPOINT)
+  response, _ := http.PostForm(TOKENS_ENDPOINT, params)
+  b, _ := ioutil.ReadAll(response.Body)
+  println(string(b))
+
+  var responseJson map[string]interface{}
+  json.Unmarshal(b, &responseJson)
+  if errorMessage, ok := responseJson["error"]; ok {
+    return os.ErrorString(errorMessage.(string))
+  } else {
+    currentTokens = &tokens{responseJson["access_token"].(string), responseJson["refresh_token"].(string)}
+    currentTokens.store()
+    return nil
+  }
+  return os.ErrorString("access_token is null")
+}
+
+func oauthGet(accessToken string, urlString string) (*http.Response, os.Error) {
+  url, _ := http.ParseURL(urlString)
+  conn, _ := net.Dial("tcp", url.Host + ":80")
+
+  clientConn := http.NewClientConn(conn, nil)
+  header := map[string][]string {"Authorization":{"OAuth " + accessToken}}
+  request := http.Request{Method:"GET", URL:url, Header:header}
+  clientConn.Write(&request)
+  return clientConn.Read(&request)
+}
+
+func getFriendList() (friends []friend, err os.Error) {
+  println("Call: " + FRIEND_LIST_ENDPOINT)
+  response, _ := oauthGet(currentTokens.AccessToken, FRIEND_LIST_ENDPOINT)
+  if response.StatusCode == 401 {
+    if err = refreshToken(); err == nil {
+      return getFriendList()
+    }
+    return nil, err
+  }
+  b, _ := ioutil.ReadAll(response.Body)
+  println(string(b))
+
+  var responseJson map[string]interface{}
+  json.Unmarshal(b, &responseJson)
+  entry := responseJson["entry"].([]interface{})
+  totalResults := int(responseJson["totalResults"].(float64))
+  itemsPerPage := int(responseJson["itemsPerPage"].(float64))
+  var itemsCount int
+  if itemsPerPage < totalResults {
+    itemsCount = itemsPerPage
+  } else {
+    itemsCount = totalResults
+  }
+  friends = make([]friend, itemsCount)
+  for i := 0; i < itemsCount; i++ {
+    data := entry[i].(map[string]interface{})
+    friends[i] = friend{
+      DisplayName:data["displayName"].(string),
+      ThumbnailUrl:data["thumbnailUrl"].(string),
+      ProfileUrl:data["profileUrl"].(string),
+    }
+  }
+  return friends, nil
+}
+
+func redirect(writer http.ResponseWriter, request *http.Request, redirectUrl string) {
+  println("Redirect to: " + redirectUrl)
+  http.Redirect(writer, request, redirectUrl, http.StatusFound)
+}
+
+func handleFriendList(writer http.ResponseWriter, request *http.Request) {
+  if request.RawURL == "/favicon.ico" { return }
+
+  var (
+    friends []friend
+    err os.Error
+  )
+
+  authorizeUrl := AUTHORIZE_URL_BASE + config["client_id"]
+  parts := strings.Split(request.RawURL, "?code=", -1)
+  if 2 <= len(parts) {
+    if err = authorizeCode(parts[1]); err != nil {
+      redirect(writer, request, authorizeUrl)
+    } else {
+      redirect(writer, request, "/")
+    }
+    return
+  } else if currentTokens == nil {
+    redirect(writer, request, authorizeUrl)
+    return
+  }
+
+  if friends, err = getFriendList(); err != nil {
+    redirect(writer, request, authorizeUrl)
+  } else {
+    params := new(struct{Friends []friend})
+    params.Friends = friends
+    tmpl, _ := template.Parse(TEMPLATE, nil)
+    tmpl.Execute(writer, params)
+  }
+}
+
+func main() {
+  var (
+    bytes []byte
+    err os.Error
+  )
+
+  if bytes, err = ioutil.ReadFile(CONFIG_FILENAME); err != nil {
+    log.Fatal("ioutil.ReadFile:", err)
+  }
+  if err = json.Unmarshal(bytes, &config); err != nil {
+    log.Fatal("json.Unmarshal:", err)
+  }
+  currentTokens = RestoreTokens()
+
+  http.Handle("/", http.HandlerFunc(handleFriendList))
+
+  addr := "localhost:" + config["redirect_port"]
+  println("Please open http://" + addr + " on a web browser.")
+  if err = http.ListenAndServe(addr, nil); err != nil {
+    log.Fatal("http.ListenAndServe:", err)
+  }
+}
