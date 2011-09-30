f1f49f2828d43658ac67e93fec0bb96c6bbe26a6
ANDO Yasushi <andyjpn@gmail.com>
2011-09-30 18:41:46 +0900

    refactoring

--- Ruby/sample.rb
+++ Ruby/sample.rb
@@ -3,52 +3,45 @@ require 'json'
 require 'net/https'
 require 'net/http'
 
-$consumer_key = "[YOUR CONSUMER KEY]"
-$consumer_secret = "[YOUR CONSUMER SECRET]"
-$redirect_uri = "[YOUR REDIRECT URI]"
 
+module Mixi
+  CONSUMER_KEY = "[YOUR CONSUMER KEY]"
+  CONSUMER_SECRET = "[YOUR CONSUMER SECRET]"
+  REDIRECT_URI = "[YOUR REDIRECT URI]"
 
-def getToken(auth_code)
-  params = "grant_type=authorization_code&client_id=#{$consumer_key}&client_secret=#{$consumer_secret}&code=#{auth_code}&redirect_uri=#{$redirect_uri}"
-  https = Net::HTTP.new('secure.mixi-platform.com',443)
-  https.use_ssl = true
-  https.start{
-   response = https.post('/2/token', params).body
-   return JSON.parse(response)
-  }
+  class Client
+    def self.example
+      raise 'authcode is mandatory.' if ARGV.empty?
+      client = Mixi::Client.new
+      client.authorize! ARGV.first
+      client.call '/people/@me/@self'
+    end
+
+    def authorize!(auth_code)
+      params = "grant_type=authorization_code&client_id=#{CONSUMER_KEY}&client_secret=#{
+        CONSUMER_SECRET}&code=#{auth_code}&redirect_uri=#{REDIRECT_URI}"
+      https = Net::HTTP.new('secure.mixi-platform.com', 443)
+      https.use_ssl = true
+      https.start do
+        response = https.post('/2/token', params).body
+        p response
+        json = JSON.parse(response)
+        @access_token = json['access_token']
+        @refresh_token = json['refresh_token']
+      end
+    end
+
+    def call(endpoint)
+      http = Net::HTTP.new('api.mixi-platform.com', 80)
+      http.start do
+        response = http.get("/2#{endpoint}",
+                            'Authorization' => "OAuth #{@access_token}",
+                            'HOST' => 'api.mixi-platform.com').body
+        p response
+        JSON.parse(response)
+      end
+    end
+  end
 end
 
-def getNewToken(refreshToken)
-  params = "grant_type=refresh_token&client_id=#{$consumer_key}&client_secret=#{$consumer_secret}&refresh_token=#{refreshToken}"
-  https = Net::HTTP.new('secure.mixi-platform.com', 443)
-  https.use_ssl = true
-  result = nil
-  https.start{
-    response = https.post('/2/token',params).body
-    result = JSON.parse(response)
-  }
-  return result
-end
-
-def request(endPoint,token)
-  http = Net::HTTP.new('api.mixi-platform.com',80)
-  result = nil
-  http.start{
-    response = http.get("/2#{endPoint}",
-                        {'Authorization' => "OAuth #{token['access_token']}",
-                        'HOST' => 'api.mixi-platform.com'}).body
-    result = JSON.parse(response)
-  }
-  return result
-end
-
-print "input Authorization code > "
-auth_code = STDIN.gets
-auth_code.chomp!
-token = getToken(auth_code)
-puts 'Access Token : '<<token['access_token']
-puts 'Refresh Token : '<<token['refresh_token']
-puts request('/people/@me/@self',token)
-token = getNewToken(token['refresh_token'])
-puts 'Access Token : '<<token['access_token']
-puts 'Refresh Token : '<<token['refresh_token']
+Mixi::Client.example
